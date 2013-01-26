2448a43ec02a9a0a4e26cee42ff007abe8f6bc38
Daniel Yang <yangsunwoo@gmail.com>
2013-01-26 15:33:05 -0500

    added edit article modal functionality
    my first fork

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -62,7 +62,7 @@ class ArticlesController < ApplicationController
 
     respond_to do |format|
       if @article.update_attributes(params[:article])
-        format.html { redirect_to(@article, :notice => 'Article was successfully updated.') }
+        format.html { redirect_to(articles_path, :notice => 'Article was successfully updated.') }
         format.xml  { head :ok }
       else
         format.html { render :action => "edit" }
