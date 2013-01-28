37ebc711a8651ca0523bcf6710b67ba165848d9e
Daniel Yang <yangsunwoo@gmail.com>
2013-01-28 16:07:17 -0500

    newly added article's edit also triggers ajax

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,13 +1,15 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-$(document).ready(function() {
+function attatchModal() {
   $('.modal').click(function(e) {
     var url = $(this).attr('href');
     var dialog_form = $('<div id="dialog-form">Loading form...</div>').dialog({
       autoOpen: false,
       width: 520,
       modal: true,
+      show: 'fade',
+      hide: 'drop',
       open: function() {
         return $(this).load(url + ' #content');
       },
@@ -18,4 +20,8 @@ $(document).ready(function() {
     dialog_form.dialog('open');
     e.preventDefault();
   });
+}
+
+$(document).ready(function() {
+  attatchModal();
 });
